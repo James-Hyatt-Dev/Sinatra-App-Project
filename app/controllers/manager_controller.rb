145d8c7518431f90a4cadbe1b8ec8c2f30524b20
prodigysurge <boatinglife6@yahoo.com>
2021-03-03 11:46:31 -0500

    resctructures views and files

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -6,59 +6,51 @@ class ManagerController < ApplicationController
     erb :'projects/show'
   end
   
-  get '/managers/new' do
-    if !manager_logged_in?
-      erb :'managers/new' 
+  get '/signup' do
+    if !logged_in?
+      erb :'/new' 
     else
-      erb :'projects/show'
+      redirect to '/projects'
     end
   end
 
-  post '/managers/new' do
+  post '/signup' do
     binding.pry
     if params[:user_name] == "" || params[:email] == "" || params[:password] == ""
-      redirect to '/managers/new'
+      redirect to '/signup'
     else
-      binding.pry
-      manager = Manager.new(:user_name => params[:user_name], :email => params[:email], :password => params[:password], :name => params[:name])
+      @manager = Manager.new(:user_name => params[:user_name], :email => params[:email], :password => params[:password], :name => params[:name])
       manager.save
-      
       session[:manager_id] = manager.id
-       
-      redirect to '/managers/login'
+      redirect to '/projects'
     end
   end
 
-  get '/managers/login' do
-    if !manager_logged_in?
-      erb :'managers/login'
+  get '/login' do
+    if !logged_in?
+      erb :'/login'
     else
-       
-      erb :'projects/show'
+      redirect to '/projects'
     end
   end
 
-  post '/managers/login' do
+  post '/login' do
     manager = Manager.find_by(:user_name => params[:user_name])
     if manager && manager.authenticate(params[:password])
-      
       session[:manager_id] = manager.id
-      redirect to '/projects/show'
+      redirect to '/projects'
     else
-      redirect to '/managers/new'
+      redirect to '/new'
     end
   end
 
-  get '/managers/logout' do
-    if manager_logged_in?
+  get '/logout' do
+    if logged_in?
         session.destroy
-        redirect to '/managers/login'
+        redirect to '/login'
     else
         redirect to '/'
     end
   end
-
-  
-
 
 end
--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,38 +1,78 @@
 class ProjectController < ApplicationController
 
-    get '/projects/new' do
-        if !manager_logged_in?
-            erb :'managers/login'
+    get '/projects' do
+        if !logged_in?
+            @projects = Projects.all
+            erb :'projects/show'
         else
-            @clients = Client.all
-            erb :'projects/new'
+            redirect to '/login'
         end
     end
     
     post '/projects/new' do
-        binding.pry
-        @project = Project.new(:name => params[:name], :task => params[:task], :content => params[:content], :client_id => params[:client_id])
-        @project.save
-        redirect to '/projects/show'
-    end
- 
-    get '/projects/show' do
-        if manager_logged_in?
-            Project.find_by_id(params[:id])
-        else 
-            erb :'/'
+        if logged_in?
+            erb :'projects/new'
+        else
+            redirect to '/login'
         end
     end
 
-    post '/projects/show' do
-        if manager_logged_in?
-        project = Project.find_by(:id => params[:id])
-            if project && project.authenticate(params[:password])
-                session[:name] = project.name
-                redirect to '/projects/show'
+    post '/projects' do
+        if logged_in?
+            if params[:content] == "" || params[:task] == "" || params[:name] == ""
+                redirect to '/projects/new'
             else
-            redirect to '/managers/new'
+                @project = current_manager.projects.build(content: params[:content] task: params[:task] name: params[:name])
+                if project.save
+                    redirect to '/projects/new'
+                end
             end
+        else
+            redirect to '/login'
         end
     end
+
+    get '/projects/:id' do
+        if logged_in?
+            @project = Project.find_by_id(params[:id])
+            erb :'projects/show_project'
+        else
+            redirect to '/login'
+        end
+    end
+
+    get '/projects/:id/edit' do
+        if logged_in?
+            @project = Project.find_by_id)params[:id])
+            if @project && @project.user == current_manager
+                erb :'projects/edit_project'
+            else
+                redirect to '/projects'
+            end
+        else
+            redirect to '/login'
+        end
+    end
+
+    patch '/projects/:id' do
+        if logged_in?
+            if params[:content] == "" || params[:task] == "" || params[:name] == ""
+                redirect to "/projects/#{params[:id]}/edit"
+          else
+            @project = Project.find_by_id(params[:id])
+            if @project && @project.manager == current_manager
+              if @project.update(content: params[:content])
+                redirect to "/projects/#{@project.id}"
+              else
+                redirect to "/projects/#{@project.id}/edit"
+              end
+            else
+              redirect to '/projects'
+            end
+          end
+        else
+          redirect to '/login'
+        end
+      end
+      
 end
